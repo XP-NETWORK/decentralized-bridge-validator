a723515c2d60bd0143423bfd5ace6b1ab0584e31
zOthix <4bstractanimation@gmail.com>
2023-10-16 13:35:00 +0500

    Update initial ts configs

--- modules/setup/index.ts
+++ modules/setup/index.ts
@@ -1,5 +1,35 @@
-const setup = () => {
-  console.log('hello')
-}
+// Setup for different environments
+const setupTestnet = () => {
+  console.log('Setting up for testnet...');
+};
 
-export { setup }
+const setupProduction = () => {
+  console.log('Setting up for production...');
+};
+
+// Display help information
+const displayHelp = () => {
+  console.log('Usage:');
+  console.log('  node yourScript.js [options]');
+  console.log('\nOptions:');
+  console.log('  --testnet         Setup for testnet environment');
+  console.log('  --help            Display this help message and exit');
+  console.log('\nBy default, without any options, the script sets up for production.');
+};
+
+const setup = () => {
+  // Check for --help flag
+  if (process.argv.includes('--help')) {
+      displayHelp();
+      return;
+  }
+
+  // Check for --testnet flag
+  if (process.argv.includes('--testnet')) {
+      setupTestnet();
+  } else {
+      setupProduction();
+  }
+};
+
+export { setup };

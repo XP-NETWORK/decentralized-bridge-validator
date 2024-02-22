04e0beec27f23d6d9a257ed1c5e3a3847cf0cd92
imsk17 <imsumitkr17@gmail.com>
2024-02-22 16:55:48 +0530

    Types: Get Rid of Unused Types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,32 +1,7 @@
-type ISecretKeyCrypto = {
-  ciphertext: string;
-  cipherparams: {
-    iv: string;
-  };
-  cipher: string;
-  kdf: string;
-  kdfparams: {
-    dklen: number;
-    salt: string;
-    n: number;
-    r: number;
-    p: number;
-  };
-  mac: string;
-};
-
-type ISecretKey = {
-  version: number;
-  kind: string;
-  id: string;
-  address: string;
-  bech32: string;
-  crypto: ISecretKeyCrypto;
-};
-
 type IMultiversXWallet = {
   password: string;
-  userWallet: ISecretKey;
+  privateKey: string;
+  address: string;
 };
 
 type ITonWallet = {
@@ -113,35 +88,6 @@ type IMultiversXChainConfig = {
   chainID: string;
 } & IChainConfig;
 
-type IEvmChainConfigAndEvmWallet = {
-  evmChainConfig: IEvmChainConfig;
-  evmWallet: IEvmWallet;
-};
-type IHederaChainConfigAndEvmWallet = {
-  hederaChainConfig: IHederaChainConfig;
-  evmWallet: IEvmWallet;
-};
-
-type ITonChainConfigAndTonWallet = {
-  tonChainConfig: ITonChainConfig;
-  tonWallet: ITonWallet;
-};
-
-type ISecretChainConfigAndSecretWallet = {
-  secretChainConfig: ISecretChainConfig;
-  secretWallet: ISecretWallet;
-};
-
-type ITezosChainConfigAndTezosWallet = {
-  tezosChainConfig: ITezosChainConfig;
-  tezosWallet: ITezosWallet;
-};
-
-type IMultiversXChainConfigAndMultiversXWallet = {
-  multiversXChainConfig: IMultiversXChainConfig;
-  multiversXWallet: IMultiversXWallet;
-};
-
 type IStakingConfig = {
   coinSymbol: string;
   coinAddress: string;
@@ -155,44 +101,6 @@ type IStakingChainConfigAndEvmWallet = {
 type IStakingChainConfigAndWallets = {
   stakingChainConfig: IStakingConfig;
   wallets: IGeneratedWallets;
-};
-type ITonContractConfig = {
-  contractAddress: string;
-  rpcURL: string;
-};
-type IEvmContractConfig = {
-  contractAddress: string;
-  rpcURL: string;
-};
-type IHederaContractConfig = {
-  contractAddress: string;
-  rpcURL: string;
-  royaltyInfoProxyAddress: string;
-};
-
-type IMultiversXContractConfig = {
-  contractAddress: string;
-  gatewayURL: string;
-};
-type ITezosContractConfig = {
-  contractAddress: string;
-  rpcURL: string;
-};
-
-type ISecretContractConfig = {
-  contractAddress: string;
-  rpcURL: string;
-  chainId: string;
-};
-
-type IEvmContractConfigAndEvmWallet = {
-  contractConfig: IEvmContractConfig;
-  evmWallet: IEvmWallet;
-};
-
-type IMultiversXContractConfigAndMultiversXWallet = {
-  contractConfig: IMultiversXContractConfig;
-  multiversXWallet: IMultiversXWallet;
 };
 
 type TChain =
@@ -214,76 +122,26 @@ export type SignerAndSignature = {
   signature: string;
 };
 
-type IBridge<TLockArgs, TClaimData> = {
-  validators: (address: string) => Promise<{ added: boolean }>;
-  validatorsCount: () => Promise<bigint>;
-  addValidator: (
-    validatorAddress: string,
-    signatures: {
-      signerAddress: string;
-      signature: string;
-    }[],
-  ) => Promise<{ hash: string; wait: () => Promise<unknown> }>;
-  lock721: (
-    lockArgs: TLockArgs,
-  ) => Promise<{ hash: string; wait: () => Promise<unknown> }>;
-  lock1155?: (
-    lockArgs: TLockArgs & { amt: bigint },
-  ) => Promise<{ hash: string; wait: () => Promise<unknown> }>;
-  claimNFT721: (
-    nftTransferData: TClaimData,
-    sigs: SignerAndSignature[],
-  ) => Promise<{ hash: string; wait: () => Promise<unknown> }>;
-  claimNFT1155?: (
-    nftTransferData: TClaimData,
-    sigs: SignerAndSignature[],
-  ) => Promise<{ hash: string; wait: () => Promise<unknown> }>;
-};
-
-type INftContract = {
-  name: (tokenId?: bigint) => Promise<string>;
-  symbol: (tokenId?: bigint) => Promise<string>;
-  royaltyInfo: (tokenId?: bigint) => Promise<string>;
-  tokenURI: (tokenId: bigint) => Promise<string>;
-  approve: (tokenId: bigint, to: string) => Promise<string>;
-};
-
 export {
-  TChain,
-  IBridge,
-  IEvmWallet,
-  IMultiversXWallet,
-  IEvmChainConfig,
-  IGeneratedWallets,
-  IConfigAndWallets,
-  IChainConfigAndWallets,
-  IChainConfig,
-  IEvmChainConfigAndEvmWallet,
-  IMultiversXChainConfigAndMultiversXWallet,
-  IStakingConfig,
-  IStakingChainConfigAndEvmWallet,
-  IEvmContractConfig,
-  IEvmContractConfigAndEvmWallet,
   IBridgeConfig,
-  IMultiversXChainConfig,
-  IMultiversXContractConfig,
-  IStakingChainConfigAndWallets,
-  IMultiversXContractConfigAndMultiversXWallet,
-  INftContract,
-  ITonWallet,
-  ITonChainConfig,
-  ITonChainConfigAndTonWallet,
-  ITonContractConfig,
-  ISecretWallet,
-  ISecretChainConfig,
-  ISecretChainConfigAndSecretWallet,
-  ISecretContractConfig,
-  ITezosWallet,
-  ITezosChainConfig,
-  ITezosChainConfigAndTezosWallet,
+  IChainConfig,
+  IChainConfigAndWallets,
+  IConfigAndWallets,
+  IEvmChainConfig,
+  IEvmWallet,
+  IGeneratedWallets,
   IHederaChainConfig,
-  IHederaChainConfigAndEvmWallet,
-  IHederaContractConfig,
   IHederaWallet,
-  ITezosContractConfig,
+  IMultiversXChainConfig,
+  IMultiversXWallet,
+  ISecretChainConfig,
+  ISecretWallet,
+  IStakingChainConfigAndEvmWallet,
+  IStakingChainConfigAndWallets,
+  IStakingConfig,
+  ITezosChainConfig,
+  ITezosWallet,
+  ITonChainConfig,
+  ITonWallet,
+  TChain,
 };

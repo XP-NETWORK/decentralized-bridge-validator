30478c21bdc11393ed8765c3f95ee39a1e8c7fae
imsk17 <imsumitkr17@gmail.com>
2024-02-26 19:48:39 +0530

    Listener: MULTIVERSX: Fix Try Catch

--- src/handler/multiversx/utils/listenForLockEvents.ts
+++ src/handler/multiversx/utils/listenForLockEvents.ts
@@ -19,90 +19,94 @@ export default async function listenForLockEvents(
   gatewayURL: string,
   em: EntityManager,
 ) {
-  try {
-    let lastBlock_ = lastBlock;
-    while (true) {
-      const txs = (
-        await gateway.get<Root>(`hyperblock/by-nonce/${lastBlock_.toString()}`)
-      ).data;
+  let lastBlock_ = lastBlock;
+  while (true) {
+    try {
+      {
+        const txs = (
+          await gateway.get<Root>(
+            `hyperblock/by-nonce/${lastBlock_.toString()}`,
+          )
+        ).data;
 
-      const txsForBridge = txs.data.hyperblock.transactions.filter(
-        (e) =>
-          (e.receiver.toLowerCase() === bridge.toLowerCase() &&
-            e.function === "lock721") ||
-          e.function === "lock1155",
-      );
-
-      if (!txsForBridge.length) {
-        MxLog(
-          `No Transactions found in chain from block: ${lastBlock_}. Waiting for 10 Seconds before looking for new transactions`,
+        const txsForBridge = txs.data.hyperblock.transactions.filter(
+          (e) =>
+            (e.receiver.toLowerCase() === bridge.toLowerCase() &&
+              e.function === "lock721") ||
+            e.function === "lock1155",
         );
-        const lastestStatus = await provider.getNetworkStatus();
-        const lastNonce = lastestStatus.HighestFinalNonce;
-        lastBlock_ = lastBlock_ + 1;
-        if (lastBlock >= lastNonce) {
-          // Sleep for 2 minutes
-          await new Promise<undefined>((e) => setTimeout(e, 2 * 60 * 1000));
+
+        if (!txsForBridge.length) {
+          MxLog(
+            `No Transactions found in chain from block: ${lastBlock_}. Waiting for 10 Seconds before looking for new transactions`,
+          );
+          const lastestStatus = await provider.getNetworkStatus();
+          const lastNonce = lastestStatus.HighestFinalNonce;
+          lastBlock_ = lastBlock_ + 1;
+          if (lastBlock >= lastNonce) {
+            // Sleep for 2 minutes
+            await new Promise<undefined>((e) => setTimeout(e, 2 * 60 * 1000));
+            continue;
+          }
+          await em.upsert(Block, {
+            chain: CHAIN_IDENT,
+            contractAddress: bridge,
+            lastBlock: Number(lastBlock_),
+          });
+          await em.flush();
+          await new Promise<undefined>((e) => setTimeout(e, 10000));
           continue;
         }
+        for (const tx of txsForBridge) {
+          const response = (
+            await axios.get(
+              `${gatewayURL.replace("gateway", "api")}/transactions/${tx.hash}`,
+            )
+          ).data;
+          const lockEvent = response.results.logs.find(
+            (e: { identifier: string }) =>
+              e.identifier === "lock721" || e.identifier === "lock1155",
+          );
+          const completed = response.results.logs.find(
+            (e: { identifier: string }) => e.identifier === "completedTxEvent",
+          );
+
+          if (!lockEvent || !completed) {
+            throw new Error("Invalid Lock Transaction");
+          }
+          const decodedLogs = decodeBase64Array(lockEvent.topics);
+          const tokenId = String(decodedLogs[1].charCodeAt(0));
+          const destinationChain = decodedLogs[2];
+          const destinationUserAddress = decodedLogs[3];
+          const sourceNftContractAddress = decodedLogs[4];
+          const tokenAmount = String(decodedLogs[5].charCodeAt(0));
+          const nftType = decodedLogs[6];
+          const sourceChain = decodedLogs[7];
+          cb(
+            builder.nftLocked(
+              tokenId,
+              destinationChain,
+              destinationUserAddress,
+              sourceNftContractAddress,
+              tokenAmount,
+              nftType,
+              sourceChain,
+              tx.hash,
+            ),
+          );
+        }
+        lastBlock_ = lastBlock_ + 1;
         await em.upsert(Block, {
           chain: CHAIN_IDENT,
           contractAddress: bridge,
           lastBlock: Number(lastBlock_),
         });
         await em.flush();
-        await new Promise<undefined>((e) => setTimeout(e, 10000));
-        continue;
       }
-      for (const tx of txsForBridge) {
-        const response = (
-          await axios.get(
-            `${gatewayURL.replace("gateway", "api")}/transactions/${tx.hash}`,
-          )
-        ).data;
-        const lockEvent = response.results.logs.find(
-          (e: { identifier: string }) =>
-            e.identifier === "lock721" || e.identifier === "lock1155",
-        );
-        const completed = response.results.logs.find(
-          (e: { identifier: string }) => e.identifier === "completedTxEvent",
-        );
-
-        if (!lockEvent || !completed) {
-          throw new Error("Invalid Lock Transaction");
-        }
-        const decodedLogs = decodeBase64Array(lockEvent.topics);
-        const tokenId = String(decodedLogs[1].charCodeAt(0));
-        const destinationChain = decodedLogs[2];
-        const destinationUserAddress = decodedLogs[3];
-        const sourceNftContractAddress = decodedLogs[4];
-        const tokenAmount = String(decodedLogs[5].charCodeAt(0));
-        const nftType = decodedLogs[6];
-        const sourceChain = decodedLogs[7];
-        cb(
-          builder.nftLocked(
-            tokenId,
-            destinationChain,
-            destinationUserAddress,
-            sourceNftContractAddress,
-            tokenAmount,
-            nftType,
-            sourceChain,
-            tx.hash,
-          ),
-        );
-      }
-
-      await em.upsert(Block, {
-        chain: CHAIN_IDENT,
-        contractAddress: bridge,
-        lastBlock: Number(lastBlock_),
-      });
-      await em.flush();
+    } catch (e) {
+      MxLog(`${e} while listening for events. Sleeping for 10 seconds`);
+      await new Promise<undefined>((resolve) => setTimeout(resolve, 10000));
     }
-  } catch (e) {
-    MxLog(`${e} while listening for ton events. Sleeping for 10 seconds`);
-    await new Promise<undefined>((resolve) => setTimeout(resolve, 10000));
   }
 }
 

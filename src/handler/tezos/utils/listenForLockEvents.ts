5700b9d1442491cb9fe44fe2bad3067d9b7399ce
imsk17 <imsumitkr17@gmail.com>
2024-02-26 19:50:56 +0530

    Listener: TEZOS: Fix Try Catch

--- src/handler/tezos/utils/listenForLockEvents.ts
+++ src/handler/tezos/utils/listenForLockEvents.ts
@@ -18,76 +18,78 @@ export default async function listenForLockEvents(
   restApiUrl: string,
   em: EntityManager,
 ) {
-  try {
-    let lastBlock = Number(lastBlock_);
-    while (true) {
-      const latestBlockNumber = (await provider.rpc.getBlockHeader()).level;
+  let lastBlock = Number(lastBlock_);
+  while (true) {
+    try {
+      {
+        const latestBlockNumber = (await provider.rpc.getBlockHeader()).level;
 
-      const latestBlock =
-        lastBlock + blockChunks < latestBlockNumber
-          ? lastBlock + blockChunks
-          : latestBlockNumber;
+        const latestBlock =
+          lastBlock + blockChunks < latestBlockNumber
+            ? lastBlock + blockChunks
+            : latestBlockNumber;
 
-      const logs = await TezosGetContractOperations({
-        fromLevel: lastBlock,
-        toLevel: latestBlock,
-        contractAddress: bridge,
-        restApiURL: restApiUrl,
-      });
-      const startBlock = lastBlock;
-      if (!logs.length) {
-        log(
-          `No Transactions found in chain TEZOS from block: ${startBlock} to: ${latestBlockNumber}. Waiting for 10 Seconds before looking for new transactions`,
-        );
+        const logs = await TezosGetContractOperations({
+          fromLevel: lastBlock,
+          toLevel: latestBlock,
+          contractAddress: bridge,
+          restApiURL: restApiUrl,
+        });
+        const startBlock = lastBlock;
+        if (!logs.length) {
+          log(
+            `No Transactions found in chain TEZOS from block: ${startBlock} to: ${latestBlockNumber}. Waiting for 10 Seconds before looking for new transactions`,
+          );
+          lastBlock = latestBlockNumber;
+          await em.upsert(Block, {
+            chain: CHAIN_IDENT,
+            contractAddress: bridge,
+            lastBlock: lastBlock,
+          });
+          await em.flush();
+          await new Promise<undefined>((e) => setTimeout(e, 10000));
+          continue;
+        }
+        for (const log of logs) {
+          const isLocked = log.tag === "locked";
+          if (!isLocked) continue;
+          const sourceNftContractAddress = extractStrOrAddr(
+            log.source_nft_address,
+          );
+
+          const {
+            token_id: tokenId, // Unique ID for the NFT transfer
+            dest_chain: destinationChain, // Chain to where the NFT is being transferred
+            dest_address: destinationUserAddress, // User's address in the destination chain
+            token_amount: tokenAmount, // amount of nfts to be transfered ( 1 in 721 case )
+            nft_type: nftType, // Sigular or multiple ( 721 / 1155)
+            source_chain: sourceChain, // Source chain of NFT
+            transaction_hash: transactionHash,
+          } = log;
+          return cb(
+            builder.nftLocked(
+              tokenId,
+              destinationChain,
+              destinationUserAddress,
+              sourceNftContractAddress,
+              tokenAmount,
+              nftType,
+              sourceChain,
+              transactionHash,
+            ),
+          );
+        }
         lastBlock = latestBlockNumber;
         await em.upsert(Block, {
           chain: CHAIN_IDENT,
           contractAddress: bridge,
           lastBlock: lastBlock,
         });
-        await em.flush();
-        await new Promise<undefined>((e) => setTimeout(e, 10000));
-        continue;
       }
-      for (const log of logs) {
-        const isLocked = log.tag === "locked";
-        if (!isLocked) continue;
-        const sourceNftContractAddress = extractStrOrAddr(
-          log.source_nft_address,
-        );
-
-        const {
-          token_id: tokenId, // Unique ID for the NFT transfer
-          dest_chain: destinationChain, // Chain to where the NFT is being transferred
-          dest_address: destinationUserAddress, // User's address in the destination chain
-          token_amount: tokenAmount, // amount of nfts to be transfered ( 1 in 721 case )
-          nft_type: nftType, // Sigular or multiple ( 721 / 1155)
-          source_chain: sourceChain, // Source chain of NFT
-          transaction_hash: transactionHash,
-        } = log;
-        return cb(
-          builder.nftLocked(
-            tokenId,
-            destinationChain,
-            destinationUserAddress,
-            sourceNftContractAddress,
-            tokenAmount,
-            nftType,
-            sourceChain,
-            transactionHash,
-          ),
-        );
-      }
-      lastBlock = latestBlockNumber;
-      await em.upsert(Block, {
-        chain: CHAIN_IDENT,
-        contractAddress: bridge,
-        lastBlock: lastBlock,
-      });
+    } catch (e) {
+      log(`${e} while listening for ton events. Sleeping for 10 seconds`);
+      await new Promise<undefined>((resolve) => setTimeout(resolve, 10000));
     }
-  } catch (e) {
-    log(`${e} while listening for ton events. Sleeping for 10 seconds`);
-    await new Promise<undefined>((resolve) => setTimeout(resolve, 10000));
   }
 }
 

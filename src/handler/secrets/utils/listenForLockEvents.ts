3f81e7b53a6e6d9235ba55c7d1676d588493dcc8
imsk17 <imsumitkr17@gmail.com>
2024-02-26 19:49:58 +0530

    Listener: SECRET: Fix Try Catch

--- src/handler/secrets/utils/listenForLockEvents.ts
+++ src/handler/secrets/utils/listenForLockEvents.ts
@@ -16,29 +16,71 @@ export default async function listenForLockEvents(
   bridge: string,
   em: EntityManager,
 ) {
-  try {
-    let lastBlock = lastBlock_;
-    while (true) {
-      const latestBlockNumberResponse =
-        await client.query.tendermint.getLatestBlock({});
-      const latestBlockNumber = Number(
-        latestBlockNumberResponse.block?.header?.height,
-      );
-
-      const latestBlock =
-        lastBlock + blockChunks < latestBlockNumber
-          ? lastBlock + blockChunks
-          : latestBlockNumber;
-
-      const query = `message.contract_address = '${bridge}' AND tx.height >= ${lastBlock} AND tx.height <= ${latestBlock}`;
-
-      const logs = await client.query.txsQuery(query);
-      const startBlock = lastBlock;
-      lastBlock = latestBlockNumber;
-      if (!logs.length) {
-        log(
-          `No Transactions found in chain from block: ${startBlock} to: ${latestBlockNumber}. Waiting for 10 Seconds before looking for new transactions`,
+  let lastBlock = lastBlock_;
+  while (true)
+    try {
+      {
+        const latestBlockNumberResponse =
+          await client.query.tendermint.getLatestBlock({});
+        const latestBlockNumber = Number(
+          latestBlockNumberResponse.block?.header?.height,
         );
+
+        const latestBlock =
+          lastBlock + blockChunks < latestBlockNumber
+            ? lastBlock + blockChunks
+            : latestBlockNumber;
+
+        const query = `message.contract_address = '${bridge}' AND tx.height >= ${lastBlock} AND tx.height <= ${latestBlock}`;
+
+        const logs = await client.query.txsQuery(query);
+        const startBlock = lastBlock;
+        lastBlock = latestBlockNumber;
+        if (!logs.length) {
+          log(
+            `No Transactions found in chain from block: ${startBlock} to: ${latestBlockNumber}. Waiting for 10 Seconds before looking for new transactions`,
+          );
+          lastBlock = latestBlockNumber;
+          await em.upsert(Block, {
+            chain: CHAIN_IDENT,
+            contractAddress: bridge,
+            lastBlock: lastBlock,
+          });
+          await em.flush();
+          await new Promise<undefined>((e) => setTimeout(e, 10000));
+          continue;
+        }
+        for (const log of logs) {
+          const logToFind = log?.jsonLog
+            ?.at(0)
+            ?.events.find((item: { type: string }) => item.type === "wasm")
+            ?.attributes.find(
+              (item: { key: string }) => item.key === "LockedEventInfo",
+            );
+          if (!logToFind) continue;
+          const parsedLog = JSON.parse(logToFind.value);
+          const {
+            token_id: tokenId, // Unique ID for the NFT transfer
+            destination_chain: destinationChain, // Chain to where the NFT is being transferred
+            destination_user_address: destinationUserAddress, // User's address in the destination chain
+            source_nft_contract_address: sourceNftContractAddress, // Address of the NFT contract in the source chain
+            token_amount: tokenAmount, // amount of nfts to be transfered ( 1 in 721 case )
+            nft_type: nftType, // Sigular or multiple ( 721 / 1155)
+            source_chain: sourceChain, // Source chain of NFT
+          } = parsedLog;
+          return cb(
+            builder.nftLocked(
+              tokenId,
+              destinationChain,
+              destinationUserAddress,
+              sourceNftContractAddress,
+              tokenAmount,
+              nftType,
+              sourceChain,
+              log.transactionHash,
+            ),
+          );
+        }
         lastBlock = latestBlockNumber;
         await em.upsert(Block, {
           chain: CHAIN_IDENT,
@@ -46,50 +88,9 @@ export default async function listenForLockEvents(
           lastBlock: lastBlock,
         });
         await em.flush();
-        await new Promise<undefined>((e) => setTimeout(e, 10000));
-        continue;
       }
-      for (const log of logs) {
-        const logToFind = log?.jsonLog
-          ?.at(0)
-          ?.events.find((item: { type: string }) => item.type === "wasm")
-          ?.attributes.find(
-            (item: { key: string }) => item.key === "LockedEventInfo",
-          );
-        if (!logToFind) continue;
-        const parsedLog = JSON.parse(logToFind.value);
-        const {
-          token_id: tokenId, // Unique ID for the NFT transfer
-          destination_chain: destinationChain, // Chain to where the NFT is being transferred
-          destination_user_address: destinationUserAddress, // User's address in the destination chain
-          source_nft_contract_address: sourceNftContractAddress, // Address of the NFT contract in the source chain
-          token_amount: tokenAmount, // amount of nfts to be transfered ( 1 in 721 case )
-          nft_type: nftType, // Sigular or multiple ( 721 / 1155)
-          source_chain: sourceChain, // Source chain of NFT
-        } = parsedLog;
-        return cb(
-          builder.nftLocked(
-            tokenId,
-            destinationChain,
-            destinationUserAddress,
-            sourceNftContractAddress,
-            tokenAmount,
-            nftType,
-            sourceChain,
-            log.transactionHash,
-          ),
-        );
-      }
-      lastBlock = latestBlockNumber;
-      await em.upsert(Block, {
-        chain: CHAIN_IDENT,
-        contractAddress: bridge,
-        lastBlock: lastBlock,
-      });
-      await em.flush();
+    } catch (e) {
+      log(`${e} while listening for events. Sleeping for 10 seconds`);
+      await new Promise<undefined>((resolve) => setTimeout(resolve, 10000));
     }
-  } catch (e) {
-    log(`${e} while listening for ton events. Sleeping for 10 seconds`);
-    await new Promise<undefined>((resolve) => setTimeout(resolve, 10000));
-  }
 }

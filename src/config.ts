51c7b2632bb3087d3382d00782ebe11ab8f40080
zOthix <4bstractanimation@gmail.com>
2024-07-23 16:32:05 +0500

    chore(config): update rpc for bsc

--- src/config.ts
+++ src/config.ts
@@ -79,7 +79,7 @@ export const prodBridgeConfig: IBridgeConfig = {
   bridgeChains: [
     {
       chain: "BSC",
-      rpcURL: "https://bsc-dataseed3.ninicoin.io",
+      rpcURL: "https://bsc-pokt.nodies.app",
       nativeCoinSymbol: "BNB",
       intialFund: "50000000000000000",
       contractAddress: "0x1704D2170683E7CC3c849Bb30cD265844dAd2B49",
@@ -125,7 +125,7 @@ export const prodBridgeConfig: IBridgeConfig = {
   },
   stakingConfig: {
     chain: "BSC",
-    rpcURL: "https://bsc-dataseed3.ninicoin.io",
+    rpcURL: "https://bsc-pokt.nodies.app",
     coinSymbol: "XPNET",
     nativeCoinSymbol: "BNB",
     chainType: "evm",

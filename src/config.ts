3c0b798d14b54067e3a12672e3f2953c10e84808
D <ddisdead462@gmail.com>
2024-09-11 18:04:54 +0500

    update(tezos): new tezos contract

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const bridgeTestChains = [
     blockChunks: 1000,
     chain: "TEZOS",
     chainType: "tezos",
-    contractAddress: "KT1UGfH2Hk2MVKt8ADqqkRqcGZussRnfpahV",
+    contractAddress: "KT1Fh6VH8BxA9xgJCn8hGZRxoo2vY3hrbqeV",
     decimals: 9,
     intialFund: "100000000",
     lastBlock: 7259145,

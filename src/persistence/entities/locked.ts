a12449da52bf2f1fc87ee0e17a6ea4348eb21f12
D <ddisdead462@gmail.com>
2024-11-07 16:36:02 +0500

    chore(db): table

--- src/persistence/entities/locked.ts
+++ src/persistence/entities/locked.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryKey, Property, Unique } from "@mikro-orm/core";
 import type { TNftTransferDetailsObject } from "xp-decentralized-sdk";
 
 @Entity({
-  tableName: "locked_event_new",
+  tableName: "locked_event",
 })
 @Unique({
   properties: ["transactionHash", "listenerChain"],
@@ -15,7 +15,7 @@ export class LockedEvent {
   tokenAmount!: bigint;
 
   @Property()
-  tokenId!: string;
+  tokenId!: bigint;
 
   @Property()
   destinationUserAddress!: string;
@@ -80,7 +80,7 @@ export class LockedEvent {
     imgUri,
     sourceNftContractAddress,
   }: TNftTransferDetailsObject) {
-    this.tokenId = tokenId;
+    this.tokenId = BigInt(tokenId);
     this.destinationUserAddress = destinationUserAddress;
     this.sourceChain = sourceChain;
     this.tokenAmount = BigInt(tokenAmount);

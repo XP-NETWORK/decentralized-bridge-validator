410e222cd5ff9317019f30b15d0ca9b59560a114
imsk17 <imsumitkr17@gmail.com>
2024-11-25 19:17:33 +0530

    feat(casper::selfIsValidator): implement method

--- src/handler/chains/casper/utils/selfIsValidator.ts
+++ src/handler/chains/casper/utils/selfIsValidator.ts
@@ -1,9 +1,20 @@
 import type { Ed25519 } from "casper-js-sdk/dist/lib/Keys";
 import type { TCasperBridgeClient } from "..";
+import { useMutexAndRelease } from "../../../utils";
 
 export default async function selfIsValidator(
-  _fetchBridge: () => Promise<readonly [TCasperBridgeClient, () => void]>,
-  _identity: Ed25519,
+  fetchBridge: () => Promise<readonly [TCasperBridgeClient, () => void]>,
+  identity: Ed25519,
 ): Promise<boolean> {
-  throw new Error("PANIC: Not yet implemented.");
+  try {
+    await useMutexAndRelease(fetchBridge, async (bridge) => {
+      return bridge.queryContractDictionary(
+        "validators",
+        Buffer.from(identity.publicKey.value()).toString("hex"),
+      );
+    });
+    return true;
+  } catch {
+    return false;
+  }
 }

b926f68a36f1f415837f330c8dace43ee93009b9
D <ddisdead462@gmail.com>
2024-12-03 15:56:03 +0500

    chore(casper): added to mainnet

--- src/config.ts
+++ src/config.ts
@@ -343,6 +343,18 @@ export const prodBridgeConfig: IBridgeConfig = {
       decimals: 18,
       blockChunks: 1000,
     },
+    {
+      chainType: "casper",
+      chain: "CASPER",
+      network: "casper",
+      contractAddress:
+        "284d7eeee5d0ece8b0d56cc7162a3cf72e6fabc62946e3a9abae219c646d56c3",
+      decimals: 9,
+      intialFund: "100000000000",
+      lastBlock: 3983916,
+      nativeCoinSymbol: "CSPR",
+      rpcURL: "https://rpc.mainnet.casperlabs.io/rpc",
+    },
   ],
   storageConfig: {
     chain: "OP",

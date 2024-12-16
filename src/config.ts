34649b5213cb0a421fb72cfb9570c3fb9c1a27fd
D <ddisdead462@gmail.com>
2024-12-16 16:44:37 +0500

    chore(secret): contract

--- src/config.ts
+++ src/config.ts
@@ -62,7 +62,7 @@ export const bridgeTestChains = [
     blockChunks: 1000,
     chainType: "scrt",
     chainId: "secret-4",
-    contractAddress: "secret1g2hwd0ah57a5elkkne544495qnfldvz02ckyg5",
+    contractAddress: "secret1ym9jslryg6r8nlch2036py92gd50vejznwhuvx",
     decimals: 6,
     intialFund: "10000000",
     lastBlock: 16833475,
